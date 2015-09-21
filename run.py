987aaef35e5a3edd3c173cdc1a17aee6ceaec573
dagutman <dagutman@gmail.com>
2015-09-20 22:49:41 -0400

    loading geojson data

--- run.py
+++ run.py
@@ -16,7 +16,9 @@ def dev_view():
 def dev_view_svg():
     return app.send_static_file('simple_svg_viewer.html')
 
-
+@app.route('/data/<path:path>')
+def send_datadir(path):
+    return send_from_directory('data', path)
 
 
 @app.route('/js/<path:path>')
