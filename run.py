5c76d7f7d52a82821bd88204f5c1efe998a2c675
dagutman <dagutman@gmail.com>
2016-02-29 16:54:37 -0500

    more webix bunglings

--- run.py
+++ run.py
@@ -3,11 +3,63 @@ from flask import Flask, send_from_directory, request,jsonify
 app = Flask(__name__)
 app.debug = True
 
+
+
+import json
 import pymongo
 ## Add in Mongo connection
 mongo_con = pymongo.MongoClient('localhost',27017)
 db_ptr = mongo_con['TileMarkup_DB']
 tile_db = db_ptr['tileLevelData']
+
+cache_db = mongo_con['dsa']
+
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 @app.route('/')
 def root():
@@ -156,6 +208,25 @@ def All_TileInfo_for_image( imageName):
     ### This returns information for all features and all tiles in a single dictinoary...
     tileData =  get_AllMarkupData_for_Image( tile_db, imageName)
     return jsonify(**tileData)
+
+
+@app.route('/webixapi/Images/')
+@crossdomain(origin='*')
+def get_cache_imageset():
+
+    imageData  = cache_db['images'].find().limit(50)
+    imageMeta = []
+
+    idx  = 0
+    for r in imageData:
+        r['id'] = idx ## It is strongly adivsed in dhtmlx to have an ID field... we probably hsold jsut use the _image id
+        idx +=1
+        img_src = 'https://isic-archive.com:443/api/v1/image/%s/thumbnail?width=256' % r['_id']
+        r['img_src'] = img_src
+        imageMeta.append(r)
+
+    #return jsonify(**imageMeta)
+    return json.dumps(imageMeta)
 
 
 ### There's cleatly a better way to do this... i don't want to install nginx on my machine right now though
