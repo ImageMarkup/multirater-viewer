6b7d0427c67e45dd90496c0240711b4e9e739d44
jobryan <jobryan>
2016-02-29 15:37:27 -0500

    modified isicload_main.py to cache images to gridfs, close issue #2

--- isic_aggregator/isic-aggregator/isicload_main.py
+++ isic_aggregator/isic-aggregator/isicload_main.py
@@ -1,29 +1,38 @@
-import requests, pymongo, json
+import requests, pymongo, json, bson, gridfs
 from isicload_api import *
 from secrets import username,password
 
 client = pymongo.MongoClient()
 db = client.dsa
+fs = gridfs.GridFS(db)
 
 login(username, password)
 studies = getStudies()
 
 def addFacets(image):
-	image["facets"] = []
-	if "clinical" in image["meta"] and "gender" in image["meta"]["clinical"]:
-		image["facets"].append("gender:" + image["meta"]["clinical"]["gender"])
-	if "clinical" in image["meta"] and "malignant" in image["meta"]["clinical"]:
-		image["facets"].append("malignant:" + image["meta"]["clinical"]["malignant"])
-	if "clinical" in image["meta"] and "quantloc" in image["meta"]["clinical"]:
-		image["facets"].append("quantloc:" + image["meta"]["clinical"]["quantloc"])
+    image["facets"] = []
+    if "clinical" in image["meta"] and "gender" in image["meta"]["clinical"]:
+        image["facets"].append("gender:" + image["meta"]["clinical"]["gender"])
+    if "clinical" in image["meta"] and "malignant" in image["meta"]["clinical"]:
+        image["facets"].append("malignant:" + image["meta"]["clinical"]["malignant"])
+    if "clinical" in image["meta"] and "quantloc" in image["meta"]["clinical"]:
+        image["facets"].append("quantloc:" + image["meta"]["clinical"]["quantloc"])
 
-	return image
+    return image
+
+def addThumbnail(image):
+    thumbnail = getImageThumbnail(image["_id"]).content
+    fs.put(thumbnail, image_id = image["_id"])
+    
+    return image
+    
 
 for study in studies:
-	print study["_id"]
-	studyImages = getStudyImages(study["_id"])
-	for image in studyImages:
-		if db.images.find({"_id": image["_id"]}).count() == 0:
-			print "\t", image["_id"]
-			newImage = addFacets(getImageDetail(image["_id"]))
-			db.images.insert_one(newImage)
+    print study["_id"]
+    studyImages = getStudyImages(study["_id"])
+    for image in studyImages:
+        if db.images.find({"_id": image["_id"]}).count() == 0:
+            print "\t", image["_id"]
+            newImage = addFacets(getImageDetail(image["_id"]))
+            addThumbnail(newImage)
+            db.images.insert_one(newImage)
