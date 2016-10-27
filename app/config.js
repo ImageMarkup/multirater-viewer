b6adac053ed4ee01febddcdca378acdc9e13d8fc
David Gutman <dagutman@gmail.com>
2016-10-27 12:10:19 -0400

    fixed slidenav issues

--- app/config.js
+++ app/config.js
@@ -3,7 +3,6 @@ define("config", function(){
 	var BASE_URL = "http://candygram.neurology.emory.edu:8080/api/v1";
 
 	var FOLDER_ID = "580ebcf0f8c2ef40353626ad";
-
 	return{
 		BASE_URL: BASE_URL,
 		FOLDER_ID: FOLDER_ID
---
+++ app/ui/slidenav.js
@@ -0,0 +1,66 @@
+define("ui/slidenav", ["config", "zoomer", "slide", "jquery"], function(config, zoomer, slide, $){
+
+	var folderName = null;
+	var patientId = null;
+
+	setDropdown = { 
+		view:"combo",  
+        placeholder:"Select Slide Set",
+        id: "slideset_list",
+        options:{
+			body:{
+				template:"#name#",
+				url: config.BASE_URL + "/folder?parentType=folder&parentId=" + config.FOLDER_ID
+			}
+        },
+        on:{
+        	"onChange": function(id){
+            	var item = this.getPopup().getBody().getItem(id);
+            	var thumbs = $$("thumbnails_panel");
+                var url = config.BASE_URL + "/folder?parentType=folder&parentId=" + item._id
+                thumbs.clearAll();
+                thumbs.load(url);
+          	},
+            "onAfterRender": function(){
+              $.get(config.BASE_URL + "/folder?parentType=folder&parentId=" + config.FOLDER_ID, function(folders){
+                  var foldersMenu = $$("slideset_list").getPopup().getList();
+                  foldersMenu.clearAll();
+                  foldersMenu.parse(folders);
+                  $$("slideset_list").setValue(folders[0].id);
+               });
+            }
+    	}
+	};
+
+	thumbnailsPanel = {
+		view: "dataview",
+        id: "thumbnails_panel",
+        select: true,
+        template: "<div class='webix_strong'>#name#</div><img src='"+ config.BASE_URL + "/file/#meta.thumbNailId#/download?.jpg'/>",
+        datatype: "json",
+        type: {height: 170, width: 200},
+        ready: function(){
+        	var item = this.getItem(this.getFirstId());
+          slide.init(item);
+        },
+        on: {
+         	"onItemClick": function(id, e, node) {
+             	item = this.getItem(id);
+             	slide.init(item);
+           	}
+      	}
+    };
+
+	nav = {
+		width: 220,
+		rows: [
+			setDropdown, 
+			thumbnailsPanel
+		]
+	};
+
+	return{
+		view: nav
+	}
+
+});
