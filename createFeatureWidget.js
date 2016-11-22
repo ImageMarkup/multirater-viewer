b4c7dc2aa6cd1a18c135b73caf72671418c44870
David Gutman <dagutman@gmail.com>
2016-11-21 22:03:25 -0500

    iterating on a feature widget

---
+++ createFeatureWidget.js
@@ -0,0 +1,833 @@
+// webix.message('hello');
+console.log('hi');
+
+
+FeatureExample = 
+
+{
+  "_id": "567420f69fc3c14cc3d0ee48",
+  "_modelType": "featureset",
+  "created": "2015-12-18T15:06:30.616000+00:00",
+  "creator": {
+    "_id": "542ee214bae4785edfa0a060",
+    "login": "brianhelba"
+  },
+  "globalFeatures": [],
+  "localFeatures": [
+    {
+      "id": "globules",
+      "name": [
+        "Globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "streaks",
+      "name": [
+        "Streaks"
+      ],
+      "type": "superpixel"
+    }
+  ],
+  "name": "ISBI Challenge 2016",
+  "version": 1
+}
+
+
+
+LongFeatureExample = 
+{
+  "_id": "55943d009fc3c13155bcad65",
+  "_modelType": "featureset",
+  "created": "2014-09-15T18:06:56+00:00",
+  "creator": {
+    "_id": "54cc1791bae47819d8e4cbca",
+    "login": "halperna"
+  },
+  "globalFeatures": [
+    {
+      "id": "pattern",
+      "name": [
+        "Pattern",
+        "Pattern"
+      ],
+      "options": [
+        {
+          "id": "diff_reticular",
+          "name": "Pattern- Diffuse reticular"
+        },
+        {
+          "id": "patchy_reticular",
+          "name": "Pattern- Patchy reticular"
+        },
+        {
+          "id": "periretic_centhypo",
+          "name": "Pattern- Peripheral reticular central hypopigmented"
+        },
+        {
+          "id": "periretic_centhyper",
+          "name": "Pattern- Peripheral reticular central hyperpigmented"
+        },
+        {
+          "id": "periretic_centglob",
+          "name": "Pattern- Peripheral reticular central globular"
+        },
+        {
+          "id": "homogen",
+          "name": "Pattern- Homogeneous"
+        },
+        {
+          "id": "peri_globules",
+          "name": "Pattern- Peripheral globular"
+        },
+        {
+          "id": "globular",
+          "name": "Pattern- Globular"
+        },
+        {
+          "id": "multi_comp",
+          "name": "Pattern- Multi-component symmetric"
+        },
+        {
+          "id": "two_comp_sym",
+          "name": "Pattern- Two component symmetric"
+        },
+        {
+          "id": "other",
+          "name": "Pattern- Other"
+        },
+        {
+          "id": "pattern_unable",
+          "name": "Pattern- Unable to determine"
+        }
+      ],
+      "type": "radio"
+    },
+    {
+      "id": "organization",
+      "name": [
+        "Organization",
+        "Organization"
+      ],
+      "options": [
+        {
+          "id": "org_yes",
+          "name": "Organized- Yes"
+        },
+        {
+          "id": "org_no",
+          "name": "Organized- No"
+        },
+        {
+          "id": "org_un",
+          "name": "Organized- Unsure"
+        }
+      ],
+      "type": "radio"
+    },
+    {
+      "id": "shapesym",
+      "name": [
+        "Organization",
+        "Shape Symmetry"
+      ],
+      "options": [
+        {
+          "id": "sym_twoaxes",
+          "name": "Symmetry- two axes"
+        },
+        {
+          "id": "sym_oneaxis",
+          "name": "Symmetry- one axis"
+        },
+        {
+          "id": "sym_none",
+          "name": "Symmetry- no axes"
+        },
+        {
+          "id": "sym_un",
+          "name": "Symmetry- Unsure"
+        }
+      ],
+      "type": "radio"
+    },
+    {
+      "id": "patternsym",
+      "name": [
+        "Organization",
+        "Pattern Symmetry"
+      ],
+      "options": [
+        {
+          "id": "symp_twoaxes",
+          "name": "Pattern Symmetry- two axes"
+        },
+        {
+          "id": "symp_oneaxis",
+          "name": "Pattern Symmetry- one axis"
+        },
+        {
+          "id": "symp_none",
+          "name": "Pattern Symmetry- no axes"
+        },
+        {
+          "id": "symp_un",
+          "name": "Pattern Symmetry- Unsure"
+        }
+      ],
+      "type": "radio"
+    },
+    {
+      "id": "arch",
+      "name": [
+        "Organization",
+        "Architectural Disorder"
+      ],
+      "options": [
+        {
+          "id": "arch_mild",
+          "name": "Architectural disorder- mild"
+        },
+        {
+          "id": "arch_mod",
+          "name": "Architectural disorder- moderate"
+        },
+        {
+          "id": "arch_marked",
+          "name": "Architectural disorder- marked"
+        }
+      ],
+      "type": "radio"
+    },
+    {
+      "id": "border",
+      "name": [
+        "Border",
+        "Border Sharpness"
+      ],
+      "options": [
+        {
+          "id": "bord_none",
+          "name": "Border- none"
+        },
+        {
+          "id": "bord_one",
+          "name": "Border- one"
+        },
+        {
+          "id": "bord_two",
+          "name": "Border- two"
+        },
+        {
+          "id": "bord_three",
+          "name": "Border- three"
+        },
+        {
+          "id": "bord_four",
+          "name": "Border- four"
+        },
+        {
+          "id": "bord_five",
+          "name": "Border- five"
+        },
+        {
+          "id": "bord_six",
+          "name": "Border- six"
+        },
+        {
+          "id": "bord_seven",
+          "name": "Border- seven"
+        },
+        {
+          "id": "bord_eight",
+          "name": "Border- eight"
+        },
+        {
+          "id": "bord_unable",
+          "name": "Border- unable to tell"
+        }
+      ],
+      "type": "radio"
+    }
+  ],
+  "localFeatures": [
+    {
+      "id": "c_lbrown",
+      "name": [
+        "Color",
+        "Color- Light brown"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_dbrown",
+      "name": [
+        "Color",
+        "Color- Dark brown"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_white",
+      "name": [
+        "Color",
+        "Color- White"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_gray",
+      "name": [
+        "Color",
+        "Color- Gray"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_blue",
+      "name": [
+        "Color",
+        "Color- Blue"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_black",
+      "name": [
+        "Color",
+        "Color- Black"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_red",
+      "name": [
+        "Color",
+        "Color- Red"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_bluegray",
+      "name": [
+        "Color",
+        "Color- Blue/gray"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "c_bluewhite",
+      "name": [
+        "Color",
+        "Color- Blue/white"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "net_typ",
+      "name": [
+        "Network",
+        "Network- Typical"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "net_atyp",
+      "name": [
+        "Network",
+        "Network- Atypical"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "net_pseudo",
+      "name": [
+        "Network",
+        "Network- Pseudonetwork"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "net_neg",
+      "name": [
+        "Network",
+        "Network- Negative"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "net_targ",
+      "name": [
+        "Network",
+        "Network- Target"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_strless",
+      "name": [
+        "Structures",
+        "Structures- Structureless"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_hypo_areas",
+      "name": [
+        "Structures",
+        "Structures- Hypo-pigented areas"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_reg_blotch",
+      "name": [
+        "Structures",
+        "Structures- Regular blotch"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irreg_blotch",
+      "name": [
+        "Structures",
+        "Structures- Irregular blotch"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_bluewhite",
+      "name": [
+        "Structures",
+        "Structures- Blue/white veil"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_bluegraygran",
+      "name": [
+        "Structures",
+        "Structures- Blue/gray granules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_scar",
+      "name": [
+        "Structures",
+        "Structures- Scar"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_peribrown",
+      "name": [
+        "Structures",
+        "Structures- peripheral brown structureless areas"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_bluegraydots",
+      "name": [
+        "Structures",
+        "Structures- Blue/Gray dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_streaks",
+      "name": [
+        "Structures",
+        "Structures- Streaks"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_pseudo",
+      "name": [
+        "Structures",
+        "Structures- Pseudopods"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_radialstream",
+      "name": [
+        "Structures",
+        "Structures- Radial streaming"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrstreaks",
+      "name": [
+        "Structures",
+        "Structures - Irregular streaks"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_lines",
+      "name": [
+        "Structures",
+        "Structures- Lines"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_circle",
+      "name": [
+        "Structures",
+        "Structures- Circles"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_clod",
+      "name": [
+        "Structures",
+        "Structures- Clods"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_chrys",
+      "name": [
+        "Structures",
+        "Structures- Chrysalis"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_rhomb",
+      "name": [
+        "Structures",
+        "Structures- Rhomboid"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regress",
+      "name": [
+        "Structures",
+        "Structures- Regresssion"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regblackdots",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Regular black dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regbrdots",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Regular brown dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrblackdots",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular black dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_periblack",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Peripheral black dots/globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrbrdots",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular brown dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrbluedots",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular blue dots"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrred",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular red"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regblackglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Regular black globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regbrglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Regular brown globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_regblueglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Regular blue globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrblackglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular black globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrbrglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular brown globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrblueglob",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular blue globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "str_irrbluered",
+      "name": [
+        "Dots and globs",
+        "Dots and globs- Irregular blue/red"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_comma",
+      "name": [
+        "Vessels",
+        "Vessels- Comma"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_atyp",
+      "name": [
+        "Vessels",
+        "Vessels- Atypical"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_pinkveil",
+      "name": [
+        "Vessels",
+        "Vessels- Pink veil (vascular blush or milky red area)"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_polymorph",
+      "name": [
+        "Vessels",
+        "Vessels- Polymorphous"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_dotted",
+      "name": [
+        "Vessels",
+        "Vessels- Dotted"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_linearbranch",
+      "name": [
+        "Vessels",
+        "Vessels- Linear-branched (arborizing)"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_serp",
+      "name": [
+        "Vessels",
+        "Vessels- Serpentine"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_hel",
+      "name": [
+        "Vessels",
+        "Vessels- Helical/Corkscrew"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_hairpin",
+      "name": [
+        "Vessels",
+        "Vessels- Hairpin/Looped"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_coil",
+      "name": [
+        "Vessels",
+        "Vessels- Coiled/Glomelular"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_milky",
+      "name": [
+        "Vessels",
+        "Vessels- Milky Red Globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_clods",
+      "name": [
+        "Vessels",
+        "Vessels- Clods"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "ves_linear",
+      "name": [
+        "Vessels",
+        "Vessels-Linear/Straight"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_lacuna",
+      "name": [
+        "Other",
+        "Other- Lacuna"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_milia",
+      "name": [
+        "Other",
+        "Other- Milia cycts"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_comedo",
+      "name": [
+        "Other",
+        "Other- Comedo-like openings"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_cereb",
+      "name": [
+        "Other",
+        "Other- Cerebriform"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_sharpdemarc",
+      "name": [
+        "Other",
+        "Other- Sharp demarcaedborder"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_spoke",
+      "name": [
+        "Other",
+        "Other- Spoke wheel"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_moth",
+      "name": [
+        "Other",
+        "Other- Moth eaten border"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_finger",
+      "name": [
+        "Other",
+        "Other- fingerprint "
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_leaf",
+      "name": [
+        "Other",
+        "Other- Maple leaf"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_ovoids",
+      "name": [
+        "Other",
+        "Other- Ovoids"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_blgrayglob",
+      "name": [
+        "Other",
+        "Other- Blue/gray globules"
+      ],
+      "type": "superpixel"
+    },
+    {
+      "id": "oth_ulcer",
+      "name": [
+        "Other",
+        "Other- Ulcer"
+      ],
+      "type": "superpixel"
+    }
+  ],
+  "name": "UDA2 Study",
+  "version": 1
+}
