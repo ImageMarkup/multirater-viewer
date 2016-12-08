cba96662fe37e11dbb1f05c6b32a64746572941f
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-12-08 22:33:33 +0000

    working features and buttons

--- app/ui/studynav.js
+++ app/ui/studynav.js
@@ -3,13 +3,11 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
     var studyName = '';
     var imageName = '';
     var slide = null;
-    //var selectedRaters = new Array();
     var selectedFeature = null;
     var featureButtons = [];
 
     pubsub.subscribe("SLIDE", function(msg, data) {
         slide = data;
-        selectedRaters = new Array();
     });
 
     var studyList = {
@@ -21,7 +19,6 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
             "onChange": function(id) {
                 var cols = [];
                 featureButtons = [];
-
                 var study = this.getPopup().getBody().getItem(id);
                 studyName = study.id;
                 var featureSetId = raterData[studyName]["FeatureSetId"];
@@ -35,7 +32,7 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
                             view:"button", 
                             width:110, 
                             badge:0, 
-                            label: feature.name[1] + "<br/>" + feature.name[0],
+                            label: feature.id.replace(new RegExp("[_|/]", 'g'), " "),
                             width:90,
                             height:50,
                             type:"iconTop",
@@ -46,8 +43,8 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
                         featureButtons.push(btn); 
                         cols.push(btn);
 
-                        if((index+1) % 2 == 0){
-                             $$("feature_list").addView({cols: cols});
+                        if((index+1) % 3 == 0 || index == data.localFeatures.length-1){
+                            $$("feature_list").addView({cols: cols});
                             cols = [];
                         }
                     });
@@ -55,98 +52,63 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
             }
         }
     };
-
-    /*var imageList = {
-        view: "combo",
-        placeholder: "Select Image",
-        id: "image_list",
-        options: [],
-        on: {
-            "onChange": function(id) {
-                //$$("raters_list").clearAll();
-                var image = this.getPopup().getBody().getItem(id);
-                imageName = image.id;
-                var raters = Object.keys(raterData[studyName]["MarkupData"][image.id]["raters"]);
-                var url = config.BASE_URL + "/resource/search?mode=prefix&types=%5B%22item%22%5D&q=" + imageName + ".jpg";
-
-                //update the slide
-                $.get(url).then(function(resource){
-                    $.get(config.BASE_URL + "/folder/" + resource.item[0].folderId, function(folder){
-                        slide.init(folder);
-                    })
-                 });
-
-                //add a box for each rater
-                var data = []
-                $.each(raters, function(index, rater){
-                    var raterColor = d3.schemeCategory20[index % 20];
-                    var spx = raterData[studyName]["MarkupData"][imageName]["raters"][rater]["meta"]["annotations"];
-                    data.push({id: rater, tiles: spx, fill: raterColor});
-                });
-                $$("raters_list").parse(data);
-
-
-            }
-        }
-    };*/
    
     var imageListDataView = {
         view: "dataview",
-        template: "<center><img src='http://candygram.neurology.emory.edu:8080/api/v1/file/582bcd90f8c2ef30f991ae8c/download?.jpg'/><div class='webix_strong'>#id#</div></center>",
+        template: "<center><div class='webix_strong'>#id#</div><img src='http://candygram.neurology.emory.edu:8080/api/v1/file/582bcd90f8c2ef30f991ae8c/download?.jpg' height='100'/></center>",
         id: "imageDataViewList",
         xCount:1,
         yCount:1,
         select: true,
         pager: "thumbPager",
-        type: {height: 170, width: 220},
+        scroll: false,
+        css: "thumbnail_cell",
+        type: {height: 150, width: 300},
         on: {
             "onItemClick": function(id) {
-                featureButtons.map(function(btn){
-                    $$(btn.id).config.badge = 0;
-                    $$(btn.id).disable();
-                    $$(btn.id).detachEvent("onItemClick");
-                    $$(btn.id).refresh();
-                });
-
+                //some initialization
                 var image = this.getItem(id);
                 imageName = image.id;
                 var raters = Object.keys(raterData[studyName]["MarkupData"][image.id]["raters"]);
                 var url = config.BASE_URL + "/resource/search?mode=prefix&types=%5B%22item%22%5D&q=" + imageName + ".jpg";
 
-                //update the slide
+                //load the slide
                 $.get(url).then(function(resource){
                     $.get(config.BASE_URL + "/folder/" + resource.item[0].folderId, function(folder){
                         slide.init(folder);
                     })
                 });
 
-                var data = [];
-                $.each(raters, function(index, rater){
-                    var ant = raterData[studyName]["MarkupData"][imageName]["raters"][rater]["meta"]["annotations"];
-                    
-                    $.each(ant, function(feature, tiles){
-                        try{
-                            $$(feature).config.badge += 1;
-                            $$(feature).enable();
-                            $$(feature).refresh();
-                        }
-                        catch(err){
-                            console.log(err);
-                        }
-                    })
-
-                    var raterColor = d3.schemeCategory20[index % 20];
-                    var spx = raterData[studyName]["MarkupData"][imageName]["raters"][rater]["meta"]["annotations"];
-                    data.push({id: rater, tiles: spx, fill: raterColor});
-                });
-
+                //reset the feature buttons
                 featureButtons.map(function(btn){
-                    if($$(btn.id).config.badge > 0){
+                    var featureRaters = raters.filter(function(raterName){
+                        var annotations = raterData[studyName]["MarkupData"][imageName]["raters"][raterName]["meta"]["annotations"];
+                        if(annotations.hasOwnProperty(btn.id) && annotations[btn.id].reduce((x,y) => x+y) > 0)
+                            return true;
+                    }).map(function(rater, index){
+                        return {
+                            id:rater, 
+                            tiles: raterData[studyName]["MarkupData"][imageName]["raters"][rater]["meta"]["annotations"],
+                            fill: d3.schemeCategory20[index % 20]
+                        }
+                    });
+
+                    console.log(featureRaters);
+
+                    $$(btn.id).config.badge = featureRaters.length;
+                    $$(btn.id).disable();
+                    $$(btn.id).detachEvent("onItemClick");
+                    console.log(featureRaters);
+
+                    if(featureRaters.length > 0){
+                        $$(btn.id).enable();
                         $$(btn.id).attachEvent("onItemClick", function(id, e, node){
                             tiles.removeOverlay();
-                            tiles.addRaterOverlays(id, data, slide.tiles);
+                            tiles.addRaterOverlays(id, featureRaters, slide.tiles);
                         });
                     }
+
+                    $$(btn.id).refresh();
                 });
             }
         }
@@ -161,36 +123,6 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
             group:1
         }
 
-    /*var imageListDataViewHolder = {
-        view: "layout",
-        rows: [  imageListDataView]
-    }*/
-
-    /*
-    var featureList = {
-        view: "datatable",
-        id: "feature_list",
-        select: "row",
-        fillspace: true,
-        columns:[
-            { id:"id", header:"Features", fillspace: true}
-        ],
-        on:{
-            OnItemClick: function(id){
-                selectedFeature = this.getItem(id).id;
-                tiles.removeOverlay();
-                tiles.addRaterOverlays(selectedFeature, selectedRaters, slide.tiles);
-            }
-        }
-    };*/
-
-    /*var featureAccordion = {
-        view: "accordion",
-        id: "feature_accordion",
-        multi: false,
-        rows: []
-    }*/
-
     var featureAccordion = {
         view: "form",
         id: "feature_list",
@@ -199,38 +131,9 @@ define("ui/studynav", ["config", "zoomer", "slide", "jquery","raterData", "tiles
         elements:[]
     }
 
-    /*var tpl = "<span style='background-color:#fill#; border-radius:4px; padding-right:10px;'>&nbsp</span>";
-    var userStudyList = {
-        view: "datatable",
-        id: "raters_list",
-        autoheight: true,
-        columns:[
-            { id:"rater_ch", header:{ content:"masterCheckbox" }, template:"{common.checkbox()}", width: 30},
-            {id:"id", header: "Raters", fillspace: true},
-            {id:"fill", header:"", editor:"color", template:tpl, width:30} 
-        ],
-        on:{
-            onCheck: function(raterId, raterBox, state){
-                //Source: http://webix.com/snippet/a57f4c5f
-                if(state){
-                    selectedRaters = $.grep(selectedRaters, function(rater, index){return rater.id == raterId}, true);
-                    selectedRaters.push($$("raters_list").getItem(raterId));
-                }
-                else{
-                    selectedRaters = $.grep(selectedRaters, function(rater, index){return rater.id == raterId}, true);
-                }
-
-                if(selectedFeature != null){
-                    tiles.removeOverlay();
-                    tiles.addRaterOverlays(selectedFeature, selectedRaters, slide.tiles);
-                }
-            }
-        }
-    };*/
-
     var studyNav = { 
         id: "study_view_tab", 
-        width: 220,
+        width: 300,
         rows:[studyList, thumbPager, imageListDataView, featureAccordion]
     };
 
